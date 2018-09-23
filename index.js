accd0a7fe61bc28250c34eb714a21612f0ae1063
Veronica Miller <vmcmiller@gmail.com>
2018-09-23 20:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "Harry Potter";
 
-const welcomeMessage = "Welcome to Flatbook, ";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser;
